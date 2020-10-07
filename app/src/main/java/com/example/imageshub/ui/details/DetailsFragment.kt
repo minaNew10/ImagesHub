8885045f26a8aa05331e408d2637d1c8219fad55
minaNew10 <minadevdream@gmail.com>
2020-10-07 17:03:13 +0200

    implement load state listener

---
+++ app/src/main/java/com/example/imageshub/ui/details/DetailsFragment.kt
@@ -0,0 +1,2 @@
+package com.example.imageshub.ui.details
+
--- app/src/main/java/com/example/imageshub/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imageshub/ui/gallery/GalleryFragment.kt
@@ -5,9 +5,11 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.example.imageshub.R
 import com.example.imageshub.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,15 +30,38 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery){
 
         binding.apply {
             gallery_recycler_view.setHasFixedSize(true)
+            //this line is to prevent the view of last elements when searching for new items
+            galleryRecyclerView.itemAnimator = null
             gallery_recycler_view.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = UnSplashPhotoLoadStateAdapter{adapter.retry()},
                 footer = UnSplashPhotoLoadStateAdapter{adapter.retry()}
-
-
             )
+            btnRetry.setOnClickListener{
+                adapter.retry()
+            }
         }
         viewModel.photos.observe(viewLifecycleOwner){
             adapter.submitData(viewLifecycleOwner.lifecycle,it)
+        }
+        adapter.addLoadStateListener {
+            loadstate ->
+                binding.apply {
+                    progressBarGalleryRecyclerView.isVisible = loadstate.source.refresh is LoadState.Loading
+                    galleryRecyclerView.isVisible = loadstate.source.refresh is LoadState.NotLoading
+                    btnRetry.isVisible = loadstate.source.refresh is LoadState.Error
+                    txtvError.isVisible = loadstate.source.refresh is LoadState.Error
+                    if(loadstate.source.refresh is LoadState.NotLoading &&
+                            // This parameter informs Pager if it should continue to make requests
+                        // for additional data in this direction
+                        // or if it should halt as the end of the dataset has been reached.
+                            loadstate.append.endOfPaginationReached && adapter.itemCount < 1){
+                        galleryRecyclerView.isVisible =false
+                        txtvError.isVisible = true
+                    }else{
+                        txtvError.isVisible = false
+                    }
+                }
+
         }
         setHasOptionsMenu(true)
 
